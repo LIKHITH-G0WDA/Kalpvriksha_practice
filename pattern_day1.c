06fc60fa6920531788894d69bfe85065c951881f
LIKHITH-G0WDA <137693558+LIKHITH-G0WDA@users.noreply.github.com>
2024-12-20 19:20:58 +0530

    Create pattern_day1.c

---
+++ pattern_day1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() 
+{
+int n,a=1;
+printf("enter:");
+scanf("%d",&n);
+for(int i=0;i<n;i++)
+{
+    for(int j=1;j<n-i+1;j++)
+    {
+        printf("%d",a++);
+    }
+    printf("\n");
+}
+printf("\n");
+int x=1;
+for(int i=0;i<n+1;i++)
+{
+    for(int j=0;j<i;j++)
+    {
+        printf("%d",x++);
+    }
+    printf("\n");
+}
+}
